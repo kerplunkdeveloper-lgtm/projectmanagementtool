5906c4d449fc1204a0431507f58b82f12c433da1
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-05-19 12:00:18 +0530

    new

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import userReducer from "../features/users/userSlice";
 import profileReducer from "../features/profile/profileSlice";
-import clientReducer from "../features/clients/clientSlice";
+import clientReducer from "../features/clients/clientslice";
 import templateReducer from "../features/template/templateSlice";
 import projectReducer from "../features/projects/projectSlice";
 import eodReportReducer from "../features/eodReports/eodReportSlice";
