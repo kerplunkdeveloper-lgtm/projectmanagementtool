fa194c22b9828d880b811723aba68e974015d79a
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-07-30 15:35:25 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import ThemeSettingsPanel from "./components/common/ThemeSettingsPanel";
 import Preloader from "./components/common/Preloader";
-import InReviewNotificationPopup from "./components/common/InReviewNotificationPopup";
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
       <Preloader />
       <AppRoutes />
       <ThemeSettingsPanel />
-      <InReviewNotificationPopup />
     </>
   );
 };
