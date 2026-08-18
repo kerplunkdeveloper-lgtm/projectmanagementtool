9fcd6a49269f73c74e1f8dc7acdf5b6009a4cf76
kerplunkdeveloper-lgtm <kerplunkdeveloper@gmail.com>
2026-08-18 17:56:10 +0530

    commit

--- frontend/src/pages/Dashboard/cards/DashboardCards.jsx
+++ frontend/src/pages/Dashboard/cards/DashboardCards.jsx
@@ -140,7 +140,7 @@ const DashboardCards = () => {
         className={`grid gap-4 ${
           cards.length === 1
             ? "grid-cols-1"
-            : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+            : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
         }`}
       >
         {cards.map((card, index) => {
